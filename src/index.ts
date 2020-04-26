d125391800b0a004be56d64b4b6044437cf6cd5e
dshuran <dmitry.shuran@gmail.com>
2020-04-26 14:46:57 +0300

    master: small refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {accessToken} from "./AccessToken";
 import {BotTests} from "./BotTests";
-import {dbManager} from "./database/DatabaseManager";
+import {dbManager} from "./DatabaseManager";
 
 //const appTests = new BotTests(accessToken.get(), '@anon_bot_channel');
 //appTests.execute()
